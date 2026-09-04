489ea386100c08492fd627840791177082ebaba2
Savdess <L26550316@chihuahua2.TECNM.MX>
2026-09-03 18:44:29 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+import java.util.*;
+import java.lang.Math;
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_SALIDAS {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Fundamentos de Programación");
+        System.out.println("Erick Esaú Riós Molinar");
+        System.out.println("Tu calificacion sera");
+        System.out.println(85);
+    }
+}
+
+
