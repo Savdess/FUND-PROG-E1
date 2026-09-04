e4a92d4c4844e988b5685fea425ec7ff0374f137
Savdess <L26550316@chihuahua2.TECNM.MX>
2026-09-03 18:44:54 -0600

    Add files via upload

---
+++ EVA_2_HOLA_MUNDO/src/eva_2_hola_mundo/EVA_2_HOLA_MUNDO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_hola_mundo;
+/*
+ *
+ * @author bisonte
+ */
+public class EVA_2_HOLA_MUNDO {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //ESTO ES UN BLOQUE DE CODIGO 
+        //Mostrar un mensaje de texto
+        //Mostrar--->Instruccion
+        //Texto---->Datos
+        System.out.println("Hola mundo");
+      System.out.print("¿Quieres seguir? (si/no): ");
+ 
+    }
+}
