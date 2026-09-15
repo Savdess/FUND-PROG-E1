da067e56e6939473b8bf960509abe64e72f639fe
Savdess <L26550316@chihuahua2.TECNM.MX>
2026-09-14 18:52:10 -0600

    Add files via upload

---
+++ EVA1_7_OPERACIONES/src/eva1_7_operaciones/EVA1_7_OPERACIONES.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_operaciones;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_7_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //EL TIPO DE DATO DETERMINA LAS OPERACIONES
+        //ENTEROS,REALES,CADENAS,LOGICOS,ETC
+        //EXPRESION;
+        //VALORES
+        //VARIABLES
+        //OPERADORES
+        //FUNCIONES
+        //QUE SE EVALUA PARA OBTENER UN RESULTADO
+        //OPERACIONES CON NOMBRE;
+        //ARITMETICAS
+        int sum,val1,val2;
+        //ASIGNACION: PONER UN VALOR EN UNA VARIABLE
+        //OPERADOR---> SIMBOLO QUE TRABAJA SOBRE IDENTIFICADORES.
+        // UN = ES UNA ASIGANCION   
+        //DOS == ES UNA COMPARACION
+        val1 = 100;//SE ASIGNA EL VALOR DE 100 DENTRO DE LA VARIABLE
+        val2 = 50;
+        sum = val1 + val2;//EXPRESION,ASIGNAR A LA VARIABLE SUMA LA OPERACION ENTRE VAL1 Y VAL 2
+        //+ ESTA ENTRE DOS ENTEROS,SE INTERPRETA COMO QUE LOS DEBE SUMAR
+        System.out.print("val1 =");
+        System.out.println(val1);
+         System.out.print("val2 =");
+        System.out.println(val2);
+         System.out.print("suma =");
+        System.out.println(sum);
+        //OPERACIONES RESPETAN REGLAS MATEMATICAAS
+        //MULTIPLICACION
+        //* 
+        int multi;
+        multi = val1 * val2;
+           System.out.print("multi =");
+        System.out.println(multi);
+        // /--->OPERADOR DE DIVISION
+        int div;
+        div = val1/val2;
+          System.out.print("div =");
+        System.out.println(div);
+        val1= 10;
+        val2= 3;
+        div = val1/val2;
+          System.out.print("val1 =");
+        System.out.println(val1);
+         System.out.print("val2 =");
+        System.out.println(val2);
+           System.out.print("div =");
+        System.out.println(div);
+        double RL;
+        RL = val1/val2;
+                  System.out.print("RL =");
+        System.out.println(RL);
+        double val1C,val2C;
+        val1C = 10;
+        val2C = 3;
+        RL = val1C/val2C;
+                         System.out.print("RL =");
+        System.out.println(RL);
+    }
+    
+}
---
+++ EVA1_8_OPERACIONES_2/src/eva1_8_operaciones_2/EVA1_8_OPERACIONES_2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciones_2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_8_OPERACIONES_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int RL,val1 = 10,val2 = 5;//DECLARANDO E INICIALIZANDO
+        //JAVA INICIALIZA EN AUTOMATICO LAS VARIABLES
+        //NUMERO EN CERO,BOLLEANOS EN FALSO
+       RL = 100 + val1 * (val1/ val2);
+        System.out.print("Resultado =");
+        System.out.println(RL);
+    }
+    
+}
